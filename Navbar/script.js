c384370f49f66208a975583077ded12f19b8221b
Supakit Prongchai <prongchais@gmail.com>
2026-03-05 10:41:14 +0700

    Update service and career dropdown

--- Navbar/script.js
+++ Navbar/script.js
@@ -2,29 +2,30 @@
 const dropdown = document.getElementById('DropdownContainer');
 const serviceclick = document.getElementById('ServiceElement');
 const servicedrop = document.getElementById('ServiceContainer');
+const careerclick = document.getElementById('CareerElement');
+const careerdrop = document.getElementById('CareerContainer');
 
 serviceclick.addEventListener('click', (event) => {
     console.log("ClickServiceElement");
     servicedrop.classList.toggle('show');
+    careerdrop.classList.remove('show');
     event.stopPropagation();
+});
+
+careerclick.addEventListener('click', (event) => {
+    console.log("ClickCareerElement");
+    careerdrop.classList.toggle('show');
+    servicedrop.classList.remove('show');
+    event.stopPropagation();
+
 });
 
 window.addEventListener('click', () => {
     if (servicedrop.classList.contains('show')) {
         servicedrop.classList.remove('show');
+    } else if (careerdrop.classList.contains('show')) {
+        careerdrop.classList.remove('show');
     }
-});
-
-
-const careerclick = document.getElementById('CareerElement');
-const careerdrop = document.getElementById('CareerContainer')
-
-
-careerclick.addEventListener('click', (event) => {
-    console.log("ClickCareerElement");
-    careerdrop.classList.toggle('show')
-    event.stopPropagation();
-
 });
 
 window.addEventListener('click', () => {
@@ -32,3 +33,4 @@ window.addEventListener('click', () => {
         careerdrop.classList.remove('show');
     }
 });
+
